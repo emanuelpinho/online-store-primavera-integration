213afe0bd2eca4103a2de4fa1e2af9564cac6c70
emanuelpinho <emanuelaspinho@gmail.com>
2014-12-15 18:30:55 +0000

    Hugo matos commit

---
+++ FirstREST/FirstREST/Scripts/controllers/details.js
@@ -0,0 +1,30 @@
+﻿app.controller('DetailsController', ['$scope', '$stateParams', 'ProductService', 'AuthenticationService', 'BasketService', function ($scope, $stateParams, ProductService, AuthenticationService, BasketService) {
+
+    
+    
+        
+
+    $scope.id = $stateParams.id;
+
+    ProductService.getProductsID($scope.id)
+        .success(function handleProductsResult(result) {
+            $scope.product = result;
+        });
+
+
+    $scope.user = {
+        CodCliente: 'VD32',
+        NomeCliente: 'AlgumNome',
+        NumContribuinte: '123123',
+        Moeda: 'EUR'
+    };
+    $scope.register = AuthenticationService.register;
+
+    $scope.basket = BasketService.basket;
+    $scope.addProduct = BasketService.addProduct;
+    $scope.incrementQuantity = BasketService.incrementQuantity;
+    $scope.decrementQuantity = BasketService.decrementQuantity;
+    $scope.isAddedToBasket = function (product) {
+        return $scope.basket.hasOwnProperty(product.CodArtigo);
+    };
+}]);
