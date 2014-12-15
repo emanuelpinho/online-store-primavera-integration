57e439b99f1780f20625b7c374ccf2e6eda5f495
Pedro Miranda <pedro.miranda@blip.pt>
2014-12-15 00:24:35 +0000

    fixy fixy

--- FirstREST/FirstREST/FirstREST/Scripts/constants/api-endpoints.js
+++
@@ -1,6 +0,0 @@
-﻿app.constant("API", {
-    host: "http://localhost:49822/api/",
-    products: "Artigos",
-    clients: "clientes",
-    order: "DocVenda"
-});
--- FirstREST/FirstREST/FirstREST/Scripts/controllers/basket.js
+++
@@ -1,9 +0,0 @@
-﻿app.controller('BasketController', ['$scope', '$state', 'AuthenticationService', 'BasketService', function ($scope, $state, AuthenticationService, BasketService) {
-
-    $scope.state = $state;
-
-    $scope.basket = BasketService.basket;
-    $scope.incrementQuantity = BasketService.incrementQuantity;
-    $scope.decrementQuantity = BasketService.decrementQuantity;
-    $scope.getNumberOfItems = BasketService.getNumberOfItems;
-}]);
